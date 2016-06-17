c2486007609628c04337144043085e7ee6dc3269
Michael Seifert <michaelseifert04@yahoo.de>
2016-06-17 15:05:24 +0200

    fixed exceptions

--- nddata/utils/console.py
+++ nddata/utils/console.py
@@ -4,7 +4,8 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from astropy.utils.console import ProgressBar
-from traitlets import TraitError
+
+from ..deps import OPT_DEPS
 
 
 __all__ = ['ProgressBar']
@@ -24,7 +25,15 @@ class ProgressBar(ProgressBar):  # pragma: no cover
         See `~astropy.utils.console.ProgressBar` for information.
     """
     def __init__(self, total_or_items, file=None):
-        try:
-            super(ProgressBar, self).__init__(total_or_items, True, file)
-        except TraitError:
+        # If traitlets is installed we can try to use the widget for the
+        # progressbar (if it fails with a traits error we can fallback to
+        # no-widget display)
+        if OPT_DEPS['TRAITLETS']:
+            from traitlets import TraitError
+            try:
+                super(ProgressBar, self).__init__(total_or_items, True, file)
+            except TraitError:
+                super(ProgressBar, self).__init__(total_or_items, False, file)
+        # No traitlets means we need to use the no-widget progressbar.
+        else:
             super(ProgressBar, self).__init__(total_or_items, False, file)
